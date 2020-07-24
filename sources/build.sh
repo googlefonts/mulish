c3f1a4dc6d2ba633e1c67ecedb19cfd96ef9a7dd
Yanone <post@yanone.de>
2020-07-24 13:46:37 +0200

    Reorganized build script, dropped hinting for VF

--- sources/build.sh
+++ sources/build.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 set -e
 
-TTFDIR=../fonts/ttf
-VFDIR=../fonts/vf
-mkdir -p $TTFDIR
-mkdir -p $VFDIR
 
 echo "Generating Static fonts"
-rm -r $TTFDIR/*.ttf
+TTFDIR=../fonts/ttf
+mkdir -p $TTFDIR
+rm -rf $TTFDIR/*.ttf
 fontmake -g Mulish.glyphs -i -o ttf --output-dir $TTFDIR -a
 
 echo "Post processing"
@@ -15,32 +13,35 @@ ttfs=$(ls $TTFDIR/*.ttf)
 for ttf in $ttfs
 do
 	gftools fix-dsig -f $ttf;
+	ttfautohint $ttf $ttf.fix
+	mv "$ttf.fix" $ttf;
 	gftools fix-hinting $ttf;
 	mv "$ttf.fix" $ttf;
 done
 
 
-echo "Generating VFs"
-rm -r $VFDIR/*.ttf
-fontmake -g temp_no_slnt_axis/Mulish.glyphs -o variable --output-path "$VFDIR/Mulish[wght].ttf"
-fontmake -g temp_no_slnt_axis/Mulish_Italic.glyphs -o variable --output-path "$VFDIR/Mulish-Italic[wght].ttf"
+# echo "Generating VFs"
+# VFDIR=../fonts/vf
+# mkdir -p $VFDIR
+# rm -rf $VFDIR/*.ttf
+# fontmake -g temp_no_slnt_axis/Mulish.glyphs -o variable --output-path "$VFDIR/Mulish[wght].ttf"
+# fontmake -g temp_no_slnt_axis/Mulish_Italic.glyphs -o variable --output-path "$VFDIR/Mulish-Italic[wght].ttf"
 
-echo "Post processing VFs"
-for f in $VFDIR/*.ttf
-do
-	echo Processing $f
+# echo "Post processing VFs"
+# for f in $VFDIR/*.ttf
+# do
+# 	echo Processing $f
 
-	# Apply manual fvar table
-	ttx -m $f Mulish_fvar.ttx
-	mv ./Mulish_fvar.ttf $f
-	# rm Mulish_fvar.ttf
+# 	# Apply manual fvar table
+# 	ttx -m $f Mulish_fvar.ttx
+# 	mv ./Mulish_fvar.ttf $f
 
-	gftools fix-dsig -f $f
-	ttfautohint $f $f.fix
-	mv $f.fix $f
-	gftools fix-unwanted-tables $f
-	gftools fix-hinting $f
-	mv $f.fix $f
-done
+# 	gftools fix-dsig -f $f
+# 	gftools fix-unwanted-tables $f
+# 	gftools fix-nonhinting $f $f.fix
+# 	mv $f.fix $f
+# done
 
+# Clean up
+rm -rf $VFDIR/*backup*.ttf
 rm -rf master_ufo/ instance_ufo/
