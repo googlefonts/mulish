b22f807d2008e3382f90c7c2608e5886b56a53e0
Yanone <post@yanone.de>
2020-07-24 13:49:34 +0200

    Update build.sh

--- sources/build.sh
+++ sources/build.sh
@@ -20,27 +20,27 @@ do
 done
 
 
-# echo "Generating VFs"
-# VFDIR=../fonts/vf
-# mkdir -p $VFDIR
-# rm -rf $VFDIR/*.ttf
-# fontmake -g temp_no_slnt_axis/Mulish.glyphs -o variable --output-path "$VFDIR/Mulish[wght].ttf"
-# fontmake -g temp_no_slnt_axis/Mulish_Italic.glyphs -o variable --output-path "$VFDIR/Mulish-Italic[wght].ttf"
+echo "Generating VFs"
+VFDIR=../fonts/vf
+mkdir -p $VFDIR
+rm -rf $VFDIR/*.ttf
+fontmake -g temp_no_slnt_axis/Mulish.glyphs -o variable --output-path "$VFDIR/Mulish[wght].ttf"
+fontmake -g temp_no_slnt_axis/Mulish_Italic.glyphs -o variable --output-path "$VFDIR/Mulish-Italic[wght].ttf"
 
-# echo "Post processing VFs"
-# for f in $VFDIR/*.ttf
-# do
-# 	echo Processing $f
+echo "Post processing VFs"
+for f in $VFDIR/*.ttf
+do
+	echo Processing $f
 
-# 	# Apply manual fvar table
-# 	ttx -m $f Mulish_fvar.ttx
-# 	mv ./Mulish_fvar.ttf $f
+	# Apply manual fvar table
+	ttx -m $f Mulish_fvar.ttx
+	mv ./Mulish_fvar.ttf $f
 
-# 	gftools fix-dsig -f $f
-# 	gftools fix-unwanted-tables $f
-# 	gftools fix-nonhinting $f $f.fix
-# 	mv $f.fix $f
-# done
+	gftools fix-dsig -f $f
+	gftools fix-unwanted-tables $f
+	gftools fix-nonhinting $f $f.fix
+	mv $f.fix $f
+done
 
 # Clean up
 rm -rf $VFDIR/*backup*.ttf
